ac4ae8af4459d70fc388a8035fd6ccac1381f8e4
dipto230 <diptoroy230305@gmail.com>
2024-12-15 03:37:57 +0600

    baner text motionfarmer added

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { easeOut, motion } from "framer-motion";
+
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 min-h-96">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+<div className='flex-1'>
+<img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      className="max-w-sm rounded-lg shadow-2xl" />
+</div>
+    <div className='flex-1'>
+    <motion.h1
+                        animate={{ x: 50 }}
+                        transition={{ duration: 2, delay: 1, ease: easeOut, repeat: Infinity }}
+                        className="text-5xl font-bold">Latest <motion.span
+                            animate={{ color: ['#ecff33', '#33ffe3', '#ff6133'] }}
+                            transition={{ duration: 1.5, repeat: Infinity }}
+                        >Jobs</motion.span> For You!</motion.h1>
+      <p className="py-6">
+      "Unlock Your Potential, Find Your Perfect Role.
+      Your Future Starts Here, Let's Build It Together!"
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
