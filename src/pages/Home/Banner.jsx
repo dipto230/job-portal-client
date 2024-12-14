ae23a77fee90daaad26dcab7ae923d7eebd9debd
dipto230 <diptoroy230305@gmail.com>
2024-12-15 03:57:02 +0600

    banner section done

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { easeOut, motion } from "framer-motion";
-
+import team1 from '../../assets/banner/team1 (1).jpg';
+import team2 from '../../assets/banner/second.jpg';
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
   <div className="hero-content flex-col lg:flex-row-reverse">
 <div className='flex-1'>
-<img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
+<motion.img
+                        src={team1}
+                        animate={{ y: [50, 100, 50] }}
+                        transition={{duration: 10, repeat: Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl" />
+                    <motion.img
+                        src={team2}
+                        animate={{ x: [100, 150, 100] }}
+                        transition={{duration: 10, delay: 5, repeat: Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl" />
 </div>
-    <div className='flex-1'>
+    <div className='flex-1 mr-9'>
     <motion.h1
                         animate={{ x: 50 }}
                         transition={{ duration: 2, delay: 1, ease: easeOut, repeat: Infinity }}
